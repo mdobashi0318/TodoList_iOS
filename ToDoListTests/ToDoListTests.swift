753c5b74f90540268089d2d6a0837098e7ed7fd5
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2020-06-08 22:30:09 +0900

    UnitTestにTodoModelの削除系のテストを追加

--- ToDoListTests/ToDoListTests.swift
+++ ToDoListTests/ToDoListTests.swift
@@ -57,5 +57,57 @@ class ToDoModelTests: XCTestCase {
         XCTAssert(!(todoModel?.createTime!.isEmpty)!, "Todo作成時間が登録されていない")
     }
     
+    
+    
+    
+    func test_DeleteModel() {
+        let addTodoDate = Format().stringFromDate(date: Date())
+        ToDoModel.addRealm(view, addValue: TableValue(id: "0", title: "UnitTest", todoDate: addTodoDate, detail: "詳細"))
+        
+        let todoModel = ToDoModel.findRealm(view, todoId: "0", createTime: nil)
+        
+        XCTAssertTrue(ToDoModel.allFindRealm(view)!.count > 0, "Todoが作成されていない")
+        XCTAssert(todoModel?.id == "0", "idが登録されていない")
+        XCTAssert(todoModel?.toDoName == "UnitTest", "Todoのタイトルが登録されていない")
+        XCTAssert(todoModel?.todoDate == addTodoDate, "　Todoの期限が登録されていない")
+        XCTAssert(todoModel?.toDo == "詳細", "　Todoの詳細が登録されていない")
+        XCTAssert(!(todoModel?.createTime!.isEmpty)!, "Todo作成時間が登録されていない")
+        
+        ToDoModel.deleteRealm(view, todoId: todoModel!.id, createTime: todoModel?.createTime) {
+            XCTAssertFalse(ToDoModel.allFindRealm(view)!.count > 0, "Todoが削除されていない")
+        }
+        
+    }
+    
+    
+    
+    func test_allDeleteModel() {
+        let addTodoDate = Format().stringFromDate(date: Date())
+        ToDoModel.addRealm(view, addValue: TableValue(id: "0", title: "UnitTest1", todoDate: addTodoDate, detail: "詳細1"))
+        sleep(1)
+        ToDoModel.addRealm(view, addValue: TableValue(id: "1", title: "UnitTest2", todoDate: addTodoDate, detail: "詳細2"))
+        
+        let todoModel1 = ToDoModel.findRealm(view, todoId: "0", createTime: nil)
+        
+        XCTAssertTrue(ToDoModel.allFindRealm(view)!.count == 2, "Todoが作成されていない")
+        XCTAssert(todoModel1?.id == "0", "idが登録されていない")
+        XCTAssert(todoModel1?.toDoName == "UnitTest1", "Todoのタイトルが登録されていない")
+        XCTAssert(todoModel1?.todoDate == addTodoDate, "　Todoの期限が登録されていない")
+        XCTAssert(todoModel1?.toDo == "詳細1", "　Todoの詳細が登録されていない")
+        XCTAssert(!(todoModel1?.createTime!.isEmpty)!, "Todo作成時間が登録されていない")
+        
+        let todoModel2 = ToDoModel.findRealm(view, todoId: "1", createTime: nil)
+        XCTAssert(todoModel2?.id == "1", "idが登録されていない")
+        XCTAssert(todoModel2?.toDoName == "UnitTest2", "Todoのタイトルが登録されていない")
+        XCTAssert(todoModel2?.todoDate == addTodoDate, "　Todoの期限が登録されていない")
+        XCTAssert(todoModel2?.toDo == "詳細2", "　Todoの詳細が登録されていない")
+        XCTAssert(!(todoModel2?.createTime!.isEmpty)!, "Todo作成時間が登録されていない")
+        
+        ToDoModel.allDeleteRealm(view) {
+            XCTAssertFalse(ToDoModel.allFindRealm(self.view)!.count > 0, "Todoが削除されていない")
+        }
+        
+    }
+    
 
 }
