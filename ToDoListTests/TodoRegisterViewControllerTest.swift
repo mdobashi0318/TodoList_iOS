8e8b97e6350a49640504ac801a5f14040fad53d0
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2020-08-26 22:53:17 +0900

    TodoRegisterViewControllerTestを復帰

--- ToDoListTests/TodoRegisterViewControllerTest.swift
+++ ToDoListTests/TodoRegisterViewControllerTest.swift
@@ -23,61 +23,52 @@ class TodoRegisterViewControllerTest: XCTestCase {
     }
 
     
-//    func test_init() {
-//        let vc = TodoRegisterViewController()
-//        
-//        XCTAssertNil(vc.getToDoModel(), "toDoModelが初期化されている")
-//        XCTAssertNil(vc.gettodoId(), "todoIdが初期化されている")
-//    }
-//    
-//    
-//    func test_ConvenienceInit() {
-//        let todoDate = Format().stringFromDate(date: Date())
-//        ToDoModel.addToDo(UIViewController(),
-//                           addValue: ToDoModel(id: "0",
-//                                               toDoName: "UnitTest",
-//                                               todoDate: todoDate,
-//                                               toDo: "詳細",
-//                                               createTime: nil)
-//        )
-//        
-//        let todoModel = ToDoModel.findToDo(UIViewController(), todoId: "0", createTime: nil)
-//        let vc = TodoRegisterViewController(todoId: todoModel!.id, createTime: todoModel?.createTime)
-//        
-//        
-//        XCTAssert(vc.gettodoId() == "0", "idが代入されていない")
-//        XCTAssert(vc.getToDoModel().id == "0", "idが代入されていない")
-//        XCTAssert(vc.getToDoModel().toDoName == "UnitTest", "Todoのタイトルが代入されていない")
-//        XCTAssert(vc.getToDoModel().todoDate == todoDate, "　Todoの期限が代入されていない")
-//        XCTAssert(vc.getToDoModel().toDo == "詳細", "　Todoの詳細が代入されていない")
-//        XCTAssert(vc.getToDoModel().createTime != nil, "Todo作成時間が代入されていない")
-//    }
-//
-//    
-//    func test_ValidateCheck() {
-//        let vc = TodoRegisterViewController()
-//        
-//        vc.addText(toDoName: nil, todoDate: nil, toDo: nil)
-//        XCTAssertFalse(vc.getValidateCheck(), "バリデーションが通っている")
-//        
-//        vc.addText(toDoName: "toDoName", todoDate: nil, toDo: nil)
-//        XCTAssertFalse(vc.getValidateCheck(), "バリデーションが通っている")
-//        
-//        vc.addText(toDoName: "toDoName", todoDate: "todoDate", toDo: nil)
-//        XCTAssertFalse(vc.getValidateCheck(), "バリデーションが通っている")
-//        
-//        vc.addText(toDoName: "toDoName", todoDate: "todoDate", toDo: "toDo")
-//        XCTAssertTrue(vc.getValidateCheck(), "バリデーションが通らない")
-//        
-//    }
-//    
-//    
-//    
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.1
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
+    func test_init() {
+        let vc = TodoRegisterViewController()
+        
+        XCTAssertNil(vc.getToDoModel(), "toDoModelが初期化されている")
+        XCTAssertNil(vc.gettodoId(), "todoIdが初期化されている")
+    }
+    
+    
+    func test_ConvenienceInit() {
+        let todoDate = Format().stringFromDate(date: Date())
+        ToDoModel.addToDo(addValue: ToDoModel(id: "0",
+                                              toDoName: "UnitTest",
+                                              todoDate: todoDate,
+                                              toDo: "詳細",
+                                              createTime: nil)) { _ in return}
+        
+        
+        let todoModel = ToDoModel.findToDo(todoId: "0", createTime: nil)
+        let vc = TodoRegisterViewController(todoId: todoModel!.id, createTime: todoModel?.createTime)
+        
+        
+        XCTAssert(vc.gettodoId() == "0", "idが代入されていない")
+        XCTAssert(vc.getToDoModel().id == "0", "idが代入されていない")
+        XCTAssert(vc.getToDoModel().toDoName == "UnitTest", "Todoのタイトルが代入されていない")
+        XCTAssert(vc.getToDoModel().todoDate == todoDate, "　Todoの期限が代入されていない")
+        XCTAssert(vc.getToDoModel().toDo == "詳細", "　Todoの詳細が代入されていない")
+        XCTAssert(vc.getToDoModel().createTime != nil, "Todo作成時間が代入されていない")
+    }
+
+    
+    func test_ValidateCheck() {
+        let vc = TodoRegisterViewController()
+        
+        vc.addText(toDoName: nil, todoDate: nil, toDo: nil)
+        XCTAssertFalse(vc.getValidateCheck(), "バリデーションが通っている")
+        
+        vc.addText(toDoName: "toDoName", todoDate: nil, toDo: nil)
+        XCTAssertFalse(vc.getValidateCheck(), "バリデーションが通っている")
+        
+        vc.addText(toDoName: "toDoName", todoDate: "todoDate", toDo: nil)
+        XCTAssertFalse(vc.getValidateCheck(), "バリデーションが通っている")
+        
+        vc.addText(toDoName: "toDoName", todoDate: "todoDate", toDo: "toDo")
+        XCTAssertTrue(vc.getValidateCheck(), "バリデーションが通らない")
+        
+    }
+    
 
 }
